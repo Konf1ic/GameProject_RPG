25eca50ddc0af4cecf1104564ae01e6c56efabda
Konf1ic <kenbui2809@gmail.com>
2026-01-27 03:28:04 +0700

    Creating GameSession (ViewModel) Class.

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels {
+    internal class GameSession {
+    }
+}
